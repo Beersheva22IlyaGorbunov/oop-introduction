d2fc8c65435ad9362b542e036b63dc6d2cb1f581
Ilya Gorbunov <ilya_211294@mail.ru>
2022-12-19 15:50:40 +0200

    HW #15 available memory through exceptions

--- oop-introduction/src/telran/memory/MemoryOperations.java
+++ oop-introduction/src/telran/memory/MemoryOperations.java
@@ -2,27 +2,24 @@ package telran.memory;
 
 public class MemoryOperations {
 	public static int getMaxAvailableMemory() {
-		boolean isWorked = false;
 		byte[] ar = null;
+		int maxAvailable = 0;
 		int left = 0;
 		int right = Integer.MAX_VALUE;
 		int middle = right / 2;
 		while (left <= right) {
-			isWorked = true;
 			ar = null;
 			try {
 				ar = new byte[middle];
-			} catch(Throwable e) {
-				isWorked = false;
-			}
-			if (isWorked) {
+				maxAvailable = middle;
 				left = middle + 1;
-			}
-			else {
+			} catch(Throwable e) {
 				right = middle - 1;
 			}
-			middle = (left + right) / 2;
+			middle = left + (right - left) / 2;
 		}
-		return middle;
+		return maxAvailable;
 	}
+	
+	
 }
--- oop-introduction/src/telran/memory/MemoryOperationsTest.java
+++ oop-introduction/src/telran/memory/MemoryOperationsTest.java
@@ -2,12 +2,15 @@ package telran.memory;
 
 import static org.junit.Assert.assertTrue;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 public class MemoryOperationsTest {
+	private static final long MGB = 1024 * 1024;
 	byte[] ar;
 	
 	@Test
+	@Disabled
 	void maxMemoryTestTest() {
 		int maxMemory = MemoryOperations.getMaxAvailableMemory();
 		ar = new byte[maxMemory];
@@ -19,5 +22,16 @@ public class MemoryOperationsTest {
 			flagException = true;
 		}
 		assertTrue(flagException);
+	}
+	
+	@Test
+	void standardMemoryMethods() {
+		Runtime runtime = Runtime.getRuntime();
+		System.out.printf("Max memory JVM may require from OS %d,"
+						+ "\ncurrent total JVM memory: %d," 
+						+ "\ncurrent free JVM memory: %d"
+						,runtime.maxMemory() / MGB
+						,runtime.totalMemory() / MGB
+						,runtime.freeMemory() / MGB);
 	}
 }
